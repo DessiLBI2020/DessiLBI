1f6f38e928abd98e13261062fbb973f5ab0b5da5
DessiLBI2020 <67937101+DessiLBI2020@users.noreply.github.com>
2021-05-20 20:51:45 +0800

    Update run_lbi.sh

--- DessiLBI/example/cifar_prune/run_lbi.sh
+++ DessiLBI/example/cifar_prune/run_lbi.sh
@@ -1,2 +1,2 @@
 mkdir data
-CUDA_VISIBLE_DEVICES=0 python pretrain_lbi_weight_norm.py --model vgg16_bn --epoch 2 --kappa 1 --mu 500 --post_mode finetune  --logname ./tblog/vgg16bn_lbi_weight_mu500_norm_0.0003 --fc_lambda 0.0003 --lbi_momentum 0 
+CUDA_VISIBLE_DEVICES=0 python pretrain_lbi_weight_norm.py --model vgg16_bn --epoch 200 --kappa 1 --mu 500 --post_mode finetune  --logname ./tblog/vgg16bn_lbi_weight_mu500_norm_0.0003 --fc_lambda 0.0003 --lbi_momentum 0 
